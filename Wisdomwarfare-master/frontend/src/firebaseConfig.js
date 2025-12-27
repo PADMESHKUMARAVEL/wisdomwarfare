14f1c3be4465bc5951ac0617c165a7ee0139977d
Padmesh <padmeshkumaravel@gmail.com>
2025-12-27 13:40:54 +0530

    firebase change

--- Wisdomwarfare-master/frontend/src/firebaseConfig.js
+++ Wisdomwarfare-master/frontend/src/firebaseConfig.js
@@ -2,8 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5olknMuO1mdZJ_25S96UXLEjtGuhj-WU",
-  authDomain: "wisdomwarfare-fg7q.vercel.app",
+   apiKey: "AIzaSyD5olknMuO1mdZJ_25S96UXLEjtGuhj-WU",
+  
+  // Use the Firebase project authDomain (from Firebase console).
+  // Also ensure this domain and your app host (localhost, vercel domain) are added
+  // in Firebase Console -> Authentication -> Authorized domains.
+  authDomain: "gamified-learning-89fa5.firebaseapp.com",
+  
   projectId: "gamified-learning-89fa5",
   storageBucket: "gamified-learning-89fa5.appspot.com",
   messagingSenderId: "372047414058",
